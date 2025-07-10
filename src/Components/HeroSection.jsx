6cfa896981dfbb352b6252a1adf337bb4ffb0b8b
Qunainb <waniqunain@gmail.com>
2025-07-10 11:57:29 +0530

    feat: Hero Section

---
+++ src/Components/HeroSection.jsx
@@ -0,0 +1,32 @@
+import heroBanner from "../assets/hero_banner.jpg";
+import heroTitle from "../assets/hero_title.png";
+import playIcon from "../assets/play_icon.png";
+import infoIcon from "../assets/info_icon.png";
+
+import "../Components/HeroSection.css";
+
+export default function HeroSection() {
+  return (
+    <div className="hero">
+      <img src={heroBanner} alt="hero-banner-image" className="banner-image" />
+      <div className="hero-caption">
+        <img src={heroTitle} alt="hero-title-image" className="caption-image" />
+        <p>
+          Discovering his ties to a secret ancient order, a young man living in
+          modern instanbul embarks on a quest to save the city from an immortal
+          enemy
+        </p>
+        <div className="hero-btns">
+          <button className="btn">
+            <img src={playIcon} alt="play-icon" />
+            Play
+          </button>
+          <button className="btn dark-btn">
+            <img src={infoIcon} alt="info-icon" />
+            More Info
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
+import HeroSection from "../Components/HeroSection";
 import Navbar from "../Components/Navbar";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <HeroSection />
     </div>
   );
 }
